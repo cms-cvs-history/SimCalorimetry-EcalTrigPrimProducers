150abbe64a4739a923245bd79b9ca3d1f0343905
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-21 03:16:16 +0000

    second run of translating configs

--- python/ecalTriggerPrimitiveDigis_CosmicsConfiguration_cff.py
+++ python/ecalTriggerPrimitiveDigis_CosmicsConfiguration_cff.py
@@ -1,14 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
-# Trigger Primitive Producer
-from SimCalorimetry.EcalTrigPrimProducers.ecalTriggerPrimitiveDigis_with_suppressed_cfi import *
-# ecal mapping
+# TP Emulator Producer:
+from Geometry.EcalMapping.EcalMapping_cfi import *
 from Geometry.EcalMapping.EcalMappingRecord_cfi import *
-# esmodule creating  EcalTPParametersRcd + corresponding empty essource
-EcalTrigPrimESProducer = cms.ESProducer("EcalTrigPrimESProducer",
-    DatabaseFile = cms.untracked.string('TPG.txt')
-)
-
+from MagneticField.Engine.volumeBasedMagneticField_cfi import *
+from CalibCalorimetry.Configuration.Ecal_FakeConditions_cff import *
+# Sources of record
 tpparams = cms.ESSource("EmptyESSource",
     recordName = cms.string('EcalTPGLinearizationConstRcd'),
     iovIsRunNotTime = cms.bool(True),
@@ -73,6 +70,35 @@ tpparams11 = cms.ESSource("EmptyESSource",
     recordName = cms.string('EcalTPGFineGrainTowerEERcd'),
     iovIsRunNotTime = cms.bool(True),
     firstValid = cms.vuint32(1)
+)
+
+tpparams12 = cms.ESSource("EmptyESSource",
+    recordName = cms.string('EcalTPGPhysicsConstRcd'),
+    iovIsRunNotTime = cms.bool(True),
+    firstValid = cms.vuint32(1)
+)
+
+# Event Setup module
+EcalTrigPrimESProducer = cms.ESProducer("EcalTrigPrimESProducer",
+    DatabaseFile = cms.untracked.string('TPG_MIPs.txt')
+)
+
+# Ecal Trig Prim module
+ecalTriggerPrimitiveDigis = cms.EDProducer("EcalTrigPrimProducer",
+    BarrelOnly = cms.bool(True),
+    TTFHighEnergyEB = cms.double(1.0),
+    InstanceEB = cms.string('ebDigis'),
+    InstanceEE = cms.string(''),
+    TTFHighEnergyEE = cms.double(1.0),
+    binOfMaximum = cms.int32(6), ## optional from release 200 on, from 1-10
+
+    Famos = cms.bool(False),
+    TTFLowEnergyEE = cms.double(1.0),
+    TcpOutput = cms.bool(False),
+    Debug = cms.bool(False),
+    Label = cms.string('ecalEBunpacker'),
+    TTFLowEnergyEB = cms.double(1.0) ## this + the following is added from 140_pre4 on
+
 )
 
 
